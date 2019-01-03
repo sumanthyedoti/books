0c08adf00b9e68a730dd7761e343f03041a362da
yedoti-sumanth <yedoti.sumanth@mountblue.io>
2019-01-03 11:45:37 +0530

    schema validation

--- server/models/users.js
+++ server/models/users.js
@@ -6,23 +6,41 @@ const { Schema } = mongoose;
 const UsersSchema = new Schema({
   userName: {
     type: String,
-    required: true,
+    required: [true, 'Username required!'],
     unique: true,
     minlength: 2,
     trim: true,
+    validate: {
+      validator(username) {
+        return /^[a-z][a-z0-9_]+$/.test(username);
+      },
+      message: 'Username is not valid. Should start with an alphabet. Use only a-z, _, 0-9!',
+    },
   },
   name: {
     type: String,
     required: true,
     minlength: 2,
     trim: true,
+    validate: {
+      validator(name) {
+        return /^[a-zA-Z][a-zA-Z\s]+$/.test(name);
+      },
+      message: 'Name is not valid. Should not contain any special characters. Use only A-Z, a-z, [space]',
+    },
   },
   email: {
     type: String,
-    required: true,
+    required: [true, 'Email required!'],
     unique: true,
     minlength: 6,
     trim: true,
+    validate: {
+      validator(email) {
+        return /\S+@\S+\.\S+/.test(email);
+      },
+      message: 'Please use valid Email!',
+    },
   },
   wantToRead: {
     type: [{
